a207def540d16706c4f56068ee823221ec2cb76c
z3zzz <zabra7946@naver.com>
2022-04-03 17:10:50 +0900

    refactor: remove duplicate caching code

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -17,34 +17,52 @@ export const fetchPlugin = (inputCode: string) => {
         };
       });
 
-      build.onLoad({ filter: /\.*/ }, async (args: any) => {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
+
         if (cachedResult) {
           console.log(`"${args.path}" was in indexedDB as cache`);
           return cachedResult;
         }
 
+        return null;
+      });
+
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
-        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+
         const escapedData = data
           .replace(/\n/g, "")
           .replace(/"/g, '\\"')
           .replace(/'/g, "\\'");
-        const contents =
-          fileType === "css"
-            ? `
+
+        const contents = `
           const styleElem = document.createElement('style');
           styleElem.innerText = "${escapedData}";
           document.head.appendChild(styleElem);
-        `
-            : data;
+        `;
+
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
+
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+
+      build.onLoad({ filter: /\.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
         await fileCache.setItem(args.path, result);
         return result;
       });
