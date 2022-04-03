287913eb0b96107e0c176ed73154088ae1b7a7a3
z3zzz <zabra7946@naver.com>
2022-04-03 09:51:45 +0900

    add: esbuild based code input box

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as esbuild from "esbuild-wasm";
+import { useState, useRef, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from "./unpkg";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const inputRef = useRef<HTMLTextAreaElement | null>(null);
+  const ref = useRef<any>();
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+    console.log({ result });
+    setCode(result.outputFiles[0].text);
+  };
+  useEffect(() => {
+    if (inputRef.current === null) {
+      return;
+    }
+    inputRef.current.focus();
+    startService();
+  }, []);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    console.log({ esbuildService: ref.current });
+  };
+
+  return (
+    <div>
+      <textarea
+        ref={inputRef}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
