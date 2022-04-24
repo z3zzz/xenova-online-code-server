9c254c160b4296c1f0c8f05b46b39e1e9ce003f6
z3zzz <zabra7946@naver.com>
2022-04-24 12:04:10 +0900

    fix: width of cell-list

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -2,14 +2,15 @@ import { Fragment } from "react";
 import CellListItem from "./cell-list-item";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import AddCell from "./add-cell";
+import "./cell-list.css";
 
 const CellList: React.FC = () => {
   const { order, data } = useTypedSelector((state) => state.cells);
-  const isNoCell = order.length === 0;
+  const hasNoCell = order.length === 0;
 
   return (
-    <div>
-      <AddCell alwaysVisible={isNoCell} prevCellId={null} />
+    <div className="cell-list">
+      <AddCell alwaysVisible={hasNoCell} prevCellId={null} />
       {order.map((id) => (
         <Fragment key={id}>
           <CellListItem data={data[id]} />
